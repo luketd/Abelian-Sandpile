525bc6e82bbcbd1080f72501c626406c04b6b5fa
mreece813 <reece91398@gmail.com>
2020-12-20 18:18:27 +0000

    Updated main/make_sandpile/sandpile

--- cpp-final-project-abs-main/project/sandpile.cpp
+++ cpp-final-project-abs-main/project/sandpile.cpp
@@ -2,99 +2,21 @@
 #include "make_sandpile.cpp"
 #include <iostream>
 
-bool stable(int array[][])
+bool stable(Sandpile sandpile)
 {
-
-  bool is_stable = false;
-  bool changed = false;
-
-  while (changed == false)
+  int size; 
+  bool is_stable = true;
+  bool is_not_stable = false;
+  size = sandpile.size(); //Size of the sandpile
+  for (int i = 0; i < size; i++) //These for loops will go through the sandpile and see if it is stable or not
   {
-
-    // current index being checked
-    for (int i = 0; i < N - 1; i++) //rows
+    for (int j = 0; j < size; j++)
     {
-      for (int j = 0; j <= i; j++) //element in row
+      if (sandpile[i][j] >= 4) //This will check if the amount of piece is 4 or more so we have to move them off
       {
-        if (array[i][j] >= 4)
-        {
-          is_stable = false;
-          changed == true;
-          array[i][j] = array[i][j] - 4;
-        }
-        else if (array[i][j] < 4)
-        {
-          is_stable = true;
-          continue;
-        }
+        return is_not_stable;
       }
     }
-
-    // right index
-    for (int i = 0; i < N - 1; i++) //  same row
-    {
-      for (int j = 1; j <= i - 1; j++) // one index right
-      {
-        if (is_stable == false)
-        {
-          array[i][j] += 1;
-        }
-        else if (is_stable == true)
-        {
-          continue;
-        }
-      }
-    }
-
-    // left index
-    for (int i = 0; i < N - 1; i++) // same row
-    {
-      for (int j = -1; j < i + 1; j++) // one index left
-      {
-        if (is_stable == false)
-        {
-          array[i][j] += 1;
-        }
-        else if (is_stable == true)
-        {
-          continue;
-        }
-      }
-    }
-
-    // up index
-    for (int i = -1; i < N - 1; i++) //one row above
-    {
-      for (int j = 0; j < i; j++) // same index
-      {
-        if (is_stable == false)
-        {
-          array[i][j] += 1;
-        }
-        else if (is_stable == true)
-        {
-          continue;
-        }
-      }
-    }
-
-    // down links
-    for (int i = 1; i < N - 1; i++) // one row below
-    {
-      for (int j = 0; j <= i; j++) // same index
-      {
-        if (is_stable == false)
-        {
-          array[i][j] += 1;
-        }
-        else if (is_stable == true)
-        {
-          continue;
-        }
-      }
-    }
+    return is_stable;
   }
-  return is_stable;
-}
-
-// define a function that takes a Sandpile by reference and returns a bool (to indicate if the Sandpile is stable)
+  
