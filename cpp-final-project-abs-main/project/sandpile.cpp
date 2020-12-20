3b3750ac13a90fe0e42c2b7ccb75a7f383d77ba6
mreece813 <reece91398@gmail.com>
2020-12-20 18:20:33 +0000

    Updated main/make_sandpile/sandpile

--- cpp-final-project-abs-main/project/sandpile.cpp
+++ cpp-final-project-abs-main/project/sandpile.cpp
@@ -4,19 +4,21 @@
 
 bool stable(Sandpile sandpile)
 {
-  int size; 
-  bool is_stable = true;
-  bool is_not_stable = false;
-  size = sandpile.size(); //Size of the sandpile
-  for (int i = 0; i < size; i++) //These for loops will go through the sandpile and see if it is stable or not
+  int N = sandpile.size(); //Size of the sandpile
+  for (int i = 0; i < N; i++) //These for loops will go through the sandpile and see if it is stable or not
   {
-    for (int j = 0; j < size; j++)
+    for (int j = 0; j < N; j++)
     {
       if (sandpile[i][j] >= 4) //This will check if the amount of piece is 4 or more so we have to move them off
       {
-        return is_not_stable;
+        return false;
+      }
+      else
+      {
+        continue;
       }
     }
-    return is_stable;
   }
+  return is_stable;
+}
   
